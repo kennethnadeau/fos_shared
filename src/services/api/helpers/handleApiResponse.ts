98fecd928aa5af253e601c5ca83d411d3e25e603
Kenneth Nadeau <kennethdev96@gmail.com>
2020-08-05 16:37:22 -0400

    update response object

--- src/services/api/helpers/handleApiResponse.ts
+++ src/services/api/helpers/handleApiResponse.ts
@@ -2,7 +2,7 @@ import { AxiosResponse } from "axios";
 
 export const handleApiResponse = (response: AxiosResponse) => {
   if (response.status === 200) {
-    return response.data;
+    return { data: response.data };
   }
   // TODO: flesh out with error-handling
   return {
