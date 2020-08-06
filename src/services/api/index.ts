0a1bf31de052cb53c228831f5efb777ce65c0650
Kenneth Nadeau <kennethdev96@gmail.com>
2020-08-06 13:28:14 -0400

    change main export back

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,10 +1,8 @@
 import Axios from "axios";
 
-import { account } from "./account";
-import { otp } from "./otp";
-
 export function configure(baseUrl: string) {
   Axios.defaults.baseURL = baseUrl;
 }
 
-export { account, otp };
+export * from "./account";
+export * from "./otp";
